26eec7d88402e28c4e58bc6a6486b388fc6eb049
KimyOsu <kimyosu@protonmail.com>
2025-09-11 02:14:46 -0300

    Adicionando fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function spring
+    bash -c ". $HOME/.sdkman/bin/sdkman-init.sh && spring $argv"
+end
+
+
+
+starship init fish | source
+zoxide init fish | source
+alias rm="rm -i"
+
+uwufetch 
+
