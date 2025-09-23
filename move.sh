6845d259df5ddd29d378bf76790c6586f70429c7
andrew <kimyosu@protonmail.com>
2025-09-23 03:00:27 -0300

    Adicionando script

--- move.sh
+++ move.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Instalando os programas..."
-local = "~/.config"
+local="~/.config/"
 sudo pacman -S imagemagick waybar neovim wofi fastfetch fish starship
 echo "Movendo arquivos para $local"
-cp -r config/* 
+cp -r config/* ~/.config
